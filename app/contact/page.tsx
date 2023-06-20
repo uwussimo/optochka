da8b9ffba2940ba4b2b397c169867c80e7eccd78
ismoil turdaliyev <ismoilturdaliyevfreelance@gmail.com>
2023-06-20 18:33:04 +0500

    Fixed: ESlint rules

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,6 +2,7 @@ export default function IndexPage() {
   return (
     <div className="flex flex-col items-center text-center justify-center w-full h-80 gap-4">
       <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
         OOO "MOLLYSHOP"
       </h1>
       <p className="max-w-[700px] text-lg text-center text-muted-foreground sm:text-xl pt-2">
